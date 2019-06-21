4022fd5a0f805f9648bcad89e4aaf5fccabd0815
VinnyDavies <vinny.davies@glasgow.ac.uk>
2019-06-21 18:06:50 +0100

    Added code for low rank GPs

---
+++ method-low-rank-gps/ExampleScript.R
@@ -0,0 +1,48 @@
+# Set Working Directories
+base_dir = "C:/Users/Vinny/work/" # set this to the folder containing the GIT repository
+data_dir = "left-ventricle-jrss-c/method-localgp/Simulations/Design4D/"
+code_dir = "left-ventricle-jrss-c/method-low-rank-gps/"
+
+# Load Libraries
+if (!require("mgcv")) install.packages("mgcv")
+if (!require("readr")) install.packages("mgcv")
+
+# Load Data
+X_train <- data.frame(read_csv(paste(base_dir,data_dir,"XTrain4D.csv",sep="")))
+X_test <- data.frame(read_csv(paste(base_dir,data_dir,"XTest4D.csv",sep="")))
+Y_train <- as.matrix(read_csv(paste(base_dir,data_dir,"YTrain4DStd.csv",sep="")))
+Y_test <- as.matrix(read_csv(paste(base_dir,data_dir,"YTest4DStd.csv",sep="")))
+X_train8 <- data.frame(X_train,1/X_train)
+colnames(X_train8)<-c("p1","p2","p3","p4","q1","q2","q3","q4")
+
+# Load Functions
+source(paste(base_dir,code_dir,"LR_GP_Functions.R",sep=""))
+
+# Train Model for Output Method
+model_list<-list()
+for(i in 1:25){
+  m = gam(Y_train[,1] ~ s(p1,p2,p3,p4,bs="gp",k=20) + s(q1,q2,q3,q4,bs="gp",k=20),data=X_train8) # k=2000 was used in practise
+  model_list[[i]] = m
+  print(paste("trained",i,"models"))
+}
+
+# general some starting points
+n_starting_points = 10
+starting_points = runif(n_starting_points*4,0.1,5)
+starting_points2 = matrix(runif(n_starting_points*4),ncol=4)
+
+# Low Rank GP, Output, Mean Squared Error (MSE), one starting point
+output_mse_results = LearnParam25.v(sqrt(starting_points),y=Y_test[1,],list_m=model_list,maxit=20,error_method="MSE",Cov_y=cov(cbind(Y_train))) 
+
+# Low Rank GP, Output, Mahalanobis Distance, one starting pointn_starting_points = 10
+output_mse_results = LearnParam25.v(sqrt(starting_points),y=Y_test[1,],list_m=model_list,maxit=20,error_method="Mahal",Cov_y=cov(cbind(Y_train))) 
+
+# Low Rank GP, Loss, Mean Squared Error (MSE), multiple starting points
+loss_mse_results<-emulate_loss(Y_test[1,],Y_train,X_train8,start_point=starting_points2,loss_method="MSE")
+
+# Low Rank GP, Loss, Mahalanobis Distance, multiple starting points
+loss_mahal_results<-emulate_loss(Y_test[1,],Y_train,X_train8,start_point=starting_points2,loss_method="Mahal")
+
+# Results for loss method need squaring
+# also need to identify which is the lowest optimised loss
+
---
+++ method-low-rank-gps/LR_GP_Functions.R
@@ -0,0 +1,73 @@
+predict_loss<-function(gam_model,theta){
+  x<-matrix(c(theta^2,1/theta^2),nrow=1)
+  x<-data.frame(x)
+  colnames(x)<-c("p1","p2","p3","p4","q1","q2","q3","q4")
+  predict(gam_model,x)
+}
+
+emulate_loss<-function(new_data,Data_Matrix,param_Matrix,start_points,loss_method,knots=20,max_it=100){
+  # create vector of differences
+  y<-t(t(Data_Matrix)-new_data)
+  inv_Cov_y<-solve(cov(Data_Matrix))
+  if(loss_method == "Mahal"){
+    output<-diag(y%*%inv_Cov_y%*%t(y))
+  }
+  else if(loss_method=="MSE"){
+    output<-c()
+    for(i in 1:nrow(y)){
+      output[i]<-log(mean(y[i,]^2))
+    }
+  }
+  else{
+    print("Error: Incorrect loss_method specified")
+  }
+  # fit gam
+  model<-gam(output ~ s(p1,p2,p3,p4,bs="gp",k=knots) + s(q1,q2,q3,q4,bs="gp",k=knots),data=param_Matrix)
+  # optimise to find solution
+  
+  EL_List<-list()
+  
+  for(i in 1:nrow(start_points)){
+    sp<-start_points[i,]
+    EL_List[[i+1]]<-optim(sp,predict_loss,gam_model=model,control = list(maxit=max_it),method="CG")
+  }
+  EL_List      
+}
+
+predict25.v<-function(param,y,list_m,error_method,Cov_y){
+  p1<-param[1]^2
+  p2<-param[2]^2
+  p3<-param[3]^2
+  p4<-param[4]^2
+  q1<-1/p1
+  q2<-1/p2
+  q3<-1/p3
+  q4<-1/p4
+  newd<-data.frame(p1=p1,p2=p2,p3=p3,p4=p4,q1=q1,q2=q2,q3=q3,q4=q4)
+  predictions<-c(predict(list_m[[1]],newd),predict(list_m[[2]],newd),predict(list_m[[3]],newd),predict(list_m[[4]],newd),predict(list_m[[5]],newd),predict(list_m[[6]],newd),predict(list_m[[7]],newd),predict(list_m[[8]],newd),predict(list_m[[9]],newd),predict(list_m[[10]],newd),predict(list_m[[11]],newd),predict(list_m[[12]],newd),predict(list_m[[13]],newd),predict(list_m[[14]],newd),predict(list_m[[15]],newd),predict(list_m[[16]],newd),predict(list_m[[17]],newd),predict(list_m[[18]],newd),predict(list_m[[19]],newd),predict(list_m[[20]],newd),predict(list_m[[21]],newd),predict(list_m[[22]],newd),predict(list_m[[23]],newd),predict(list_m[[24]],newd),predict(list_m[[25]],newd))
+  if(error_method=="MSE"){
+    MSE<-c(log(mean((y-predictions)^2)))
+    return(MSE=MSE) # log MSE
+  }
+  else if(error_method=="Mahal"){
+    Mahal<-log(t(y-predictions)%*%solve(Cov_y)%*%(y-predictions))
+    return(Mahal=Mahal)
+  }
+  else{
+    print("ERROR: Incorrect error_method selected")
+  }
+}
+
+LearnParam25.v<-function(param0,y,list_m,maxit=500,method="CG",error_method,Cov_y){
+  if(error_method=="MSE"){
+    opt<-optim(param0,predict25.v,method=method,control = list(maxit=maxit),y=y,list_m=list_m,error_method="MSE",Cov_y=Cov_y)
+    return(list(opt=opt))
+  }
+  else if(error_method=="Mahal"){
+    opt<-optim(param0,predict25.v,method=method,control = list(maxit=maxit),y=y,list_m=list_m,error_method="Mahal",Cov_y=Cov_y)
+    return(list(opt=opt))
+  }
+  else{
+    print("ERROR: Incorrect error_method selected")
+  }
+}
