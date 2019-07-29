0de77f19cd643d188908f9460b7a298b0dd73f98
Umberto Noè <9914422+unoe@users.noreply.github.com>
2019-07-29 16:56:12 +0200

    Update README.m

--- method-localgp/README.m
+++ method-localgp/README.m
@@ -6,17 +6,18 @@
 % Add folders to path
 startup;
 
-% 'test': data from test set
-% 'hv': data from healthy volunteer
+% 'test': data from test set (100 data points)
+% 'hv': data from healthy volunteer (1 data point)
 data_label = 'test';
 
 % Create the right Results folder, see at the end of lv.run_emulation_*
 % Example: method-localgp/Results/*
 
-% For independent parallel jobs data_id should be the PBS array ID.
-% See in the Cluster folder
+% For a given dataset data_label, identify the data point.
+% This allows for estimation in parallel independent jobs on a computer cluster, 
+% by setting the data_id to the PBS array ID.
+% For data_label = 'test', data_id can be 1, ..., 100. For data_label = 'hv', data_id = 1.
 data_id = 1;
 
 % Run emulation
 lv.run_emulation_log_loss(data_label, data_id);
-
